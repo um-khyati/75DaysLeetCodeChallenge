f6e6ba9652ad400de0d96a0ddec48e786b4a668d
Khyati Uttam <ukhyati05@gmail.com>
2026-03-13 19:00:04 +0530

    Time: 21 ms (35.51%), Space: 94.4 MB (25.55%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> seen = new HashMap<>();
+        for (int num : nums) {
+            if (seen.containsKey(num) && seen.get(num) >= 1)
+                return true;
+            seen.put(num, seen.getOrDefault(num, 0) + 1);
+        }
+        return false;
+    }
+}
