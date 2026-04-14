b70514a7a5fdf518e3ca28525eecd9f245673c51
Khyati Uttam <ukhyati05@gmail.com>
2026-04-14 21:16:58 +0530

    Time: 88 ms (20.97%), Space: 46.2 MB (58.06%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
@@ -1,45 +1,24 @@
-import java.util.*;
-
 class Solution {
-    long[][] dp;
-
+    int[] robopos;
+    Long[][] dp;
     public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
         Collections.sort(robot);
-        Arrays.sort(factory, (a, b) -> a[0] - b[0]);
-
-        int n = robot.size();
-        int m = factory.length;
-
-        dp = new long[n][m];
-        for (long[] row : dp) Arrays.fill(row, -1);
-
-        return solve(0, 0, robot, factory);
+        this.robopos = new int[robot.size()];
+        dp = new Long[robot.size()][factory.length];
+        for(int i=0;i<robot.size();i++) robopos[i] = robot.get(i);
+        Arrays.sort(factory,(a,b)->a[0]-b[0]);
+        return dfs(robot.size()-1,factory.length-1,factory);
     }
-
-    private long solve(int i, int j, List<Integer> robot, int[][] factory) {
-        int n = robot.size();
-        int m = factory.length;
-
-        // all robots assigned
-        if (i == n) return 0;
-
-        // no factory left
-        if (j == m) return (long)1e15;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        long res = solve(i, j + 1, robot, factory); // skip factory
-
-        long cost = 0;
-        int pos = factory[j][0];
-        int limit = factory[j][1];
-
-        // assign k robots to this factory
-        for (int k = 0; k < limit && i + k < n; k++) {
-            cost += Math.abs(robot.get(i + k) - pos);
-            res = Math.min(res, cost + solve(i + k + 1, j + 1, robot, factory));
+    public long dfs(int m, int n,int[][] factory){
+        if(m<0) return 0;
+        if(n<0) return Long.MAX_VALUE/200;
+        if(dp[m][n]!=null) return dp[m][n];
+        long res = dfs(m,n-1,factory);
+        long dist = 0;
+        for(int k=0;k<factory[n][1] && m-k>=0;k++){
+            dist+=Math.abs(factory[n][0]-robopos[m-k]);
+            res = Math.min(res,dist+dfs(m-k-1,n-1,factory));
         }
-
-        return dp[i][j] = res;
+        return dp[m][n] = res;
     }
 }
