9cc95f8ea60766ddda3cbc80b38f7852f709d46f
Khyati Uttam <ukhyati05@gmail.com>
2026-04-14 21:17:40 +0530

    Time: 96 ms (17.74%), Space: 45.9 MB (64.52%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
@@ -1,24 +1,26 @@
 class Solution {
-    int[] robopos;
-    Long[][] dp;
-    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
-        Collections.sort(robot);
-        this.robopos = new int[robot.size()];
-        dp = new Long[robot.size()][factory.length];
-        for(int i=0;i<robot.size();i++) robopos[i] = robot.get(i);
-        Arrays.sort(factory,(a,b)->a[0]-b[0]);
-        return dfs(robot.size()-1,factory.length-1,factory);
-    }
-    public long dfs(int m, int n,int[][] factory){
-        if(m<0) return 0;
-        if(n<0) return Long.MAX_VALUE/200;
-        if(dp[m][n]!=null) return dp[m][n];
-        long res = dfs(m,n-1,factory);
-        long dist = 0;
-        for(int k=0;k<factory[n][1] && m-k>=0;k++){
-            dist+=Math.abs(factory[n][0]-robopos[m-k]);
-            res = Math.min(res,dist+dfs(m-k-1,n-1,factory));
+    public long minimumTotalDistance(List<Integer> r, int[][] f) {
+        Collections.sort(r);
+        Arrays.sort(f, (x, y) -> x[0] - y[0]);
+        int R = r.size();
+        int F = f.length;
+        long[][] dp = new long[R + 1][F + 1];
+        for (int i = 0; i < R; i++) dp[i][F] = Long.MAX_VALUE / 4;
+        for (int j = F - 1; j >= 0; j--) {
+            long distSum = 0;
+            ArrayDeque<long[]> dq = new ArrayDeque<>();
+            dq.addLast(new long[]{R, 0});
+            for (int i = R - 1; i >= 0; i--) {
+                distSum += Math.abs(r.get(i) - f[j][0]);
+                while (!dq.isEmpty() && dq.peekFirst()[0] > i + f[j][1])
+                    dq.pollFirst();
+                long val = dp[i][j + 1] - distSum;
+                while (!dq.isEmpty() && dq.peekLast()[1] >= val)
+                    dq.pollLast();
+                dq.addLast(new long[]{i, val});
+                dp[i][j] = dq.peekFirst()[1] + distSum;
+            }
         }
-        return dp[m][n] = res;
+        return dp[0][0];
     }
 }
